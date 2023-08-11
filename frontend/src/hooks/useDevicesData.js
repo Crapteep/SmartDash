6f61cecbde58e20ff50ca584ceb0834243327f37
Crapteep <crapteep@gmail.com>
2023-08-11 20:17:39 +0200

    a hook was created to download the list of devices

---
+++ frontend/src/hooks/useDevicesData.js
@@ -0,0 +1,18 @@
+import { useQuery } from "react-query";
+import axios from "axios";
+
+const URL = process.env.REACT_APP_API_URL;
+
+const usePostsData = () => {
+  return useQuery({
+    queryKey: "devices",
+    queryFn: async () => {
+      const { data } = await axios.get(`${URL}/devices`);
+      return data;
+    },
+    refetchIntervalInBackground: true,
+    refetchOnWindowFocus: true,
+  });
+};
+
+export default usePostsData;
