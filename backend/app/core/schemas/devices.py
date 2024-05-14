187e144c0a2006a0c0e56e4b328e8e48f8ad106e
Crapteep <crapteep@gmail.com>
2024-05-14 20:38:57 +0200

    update of models for devices

--- backend/app/core/schemas/devices.py
+++ backend/app/core/schemas/devices.py
@@ -1,42 +1,68 @@
-from pydantic import BaseModel, Field
+from pydantic import BaseModel, Field, validator
 from datetime import datetime
 from bson import ObjectId
-from .virtual_pins import VirtualPinBase
+# from .virtual_pins import VirtualPinBase
+from .elements import ButtonCreate, ChartCreate, SwitchCreate, LabelCreate
 from typing import Optional
+from fastapi import Depends
+from ..utils.validators import Validator
+
 
 
 class DeviceConfiguration(BaseModel):
     serial_port: int
 
-class DeviceDashboard(BaseModel):
-    layout: list[dict] | None = None
-    elements: list[str] | None = None
+
+class LayoutItem(BaseModel):
+    element_id: str | None = None
+    w: int | None = None
+    h: int | None = None
+    x: int | None = None
+    y: int | None = None
+    i: str | None = None
+    minW: int | None = None
+    maxW: int | None = None
+    minH: int | None = None
+    maxH:int | None = None
+    moved: bool | None = None
+    static: bool | None = None
+
 
 class UpdateDashboard(BaseModel):
     device_id: str
-    layout: list[dict]
-    elements: list[str] | None = None
+    layout: list
+    elements: list[ChartCreate | ButtonCreate | SwitchCreate | LabelCreate]
+
+    @validator('device_id')
+    def validate_device_id(cls, v):
+        return Validator.is_valid_object_id(v)
 
 
 class DeviceCreate(BaseModel):
     name: str
     hardware: str
     configuration: DeviceConfiguration
-    dashboard: DeviceDashboard | None = None
+    description: str | None = None
+    layout: list[dict] | None = None
     user_id: str | None = None
     access_token: str | None = None
-    created_at: datetime = datetime.now()
+    created_at: datetime = datetime.now()   
+
 
 class Device(DeviceCreate):
     _id: str
     
 
-
 class DeviceUpdate(BaseModel):
     name: str
     hardware: str
     configuration: DeviceConfiguration
+    description: str
 
 
 class DeviceResponse(DeviceCreate):
     _id: str = Field(..., alias="id")
+
+
+
+    
