4f0ea96ec500b5cc0f44916a05c19bdab57e37aa
Crapteep <crapteep@gmail.com>
2024-05-14 22:26:24 +0200

    minor cosmetic improvements

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,10 @@
-import { useState } from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { useState, useEffect } from "react";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Navigate,
+} from "react-router-dom";
 import Topbar from "./pages/global/Topbar";
 import Sidebar from "./pages/global/Sidebar";
 import Dashboard from "./pages/dashboard";
@@ -7,51 +12,63 @@ import FAQ from "./pages/faq";
 import { CssBaseline, ThemeProvider } from "@mui/material";
 import { ColorModeContext, useMode } from "./theme";
 import Devices from "./pages/devices";
+import Datastream from "./pages/datastream";
 import Login from "./pages/login";
 import Register from "./pages/register";
 import PrivateRoutes from "./components/PrivateRoutes";
-import { Provider } from 'react-redux';
-import store from './redux/store'
+import { Provider } from "react-redux";
+import store from "./redux/store";
+import Settings from "./pages/settings/Settings";
+import { UserProvider } from "./providers/UserProvider";
+import Test from "./pages/test";
 
-
-function LoginPage({ children }) {
-  const [isLoggedIn, setIsLoggedIn] = useState(false);
-
-  return isLoggedIn ? (
-    children
-  ) : (
-    <div>
-      <Login></Login>
-      <button onClick={() => setIsLoggedIn(true)}>Log in</button>
-    </div>
-  );
-}
 
 function App() {
   const [theme, colorMode] = useMode();
   const [isSidebar, setIsSidebar] = useState(true);
+  const [isLoggedIn, setIsLoggedIn] = useState(() => {
+    return localStorage.getItem("isLoggedIn") === "true";
+  });
+
+  useEffect(() => {
+    localStorage.setItem("isLoggedIn", isLoggedIn);
+  }, [isLoggedIn]);
 
   return (
     <ColorModeContext.Provider value={colorMode}>
       <ThemeProvider theme={theme}>
         <CssBaseline />
         <Provider store={store}>
-          <div className="app">
-            <Sidebar isSidebar={isSidebar} />
-            <main className="content">
-              <Topbar setIsSidebar={setIsSidebar} />
-              <Routes>
-                <Route element={<PrivateRoutes />}>
-                  <Route path="/" element={<Dashboard />} />
-                  <Route path="/devices" element={<Devices />} />
-                  <Route path="/faq" element={<FAQ />} />
-                </Route>
-                <Route element={<Login/>} path="/login"/>
-                <Route element={<Register/>} path="/register"/>
-              </Routes>
-            </main>
-          </div>
-          </Provider>
+          <UserProvider>
+            <div className="app">
+              {isLoggedIn && <Sidebar isSidebar={isSidebar} />}
+              <main className="content">
+                {isLoggedIn && (
+                  <Topbar
+                    setIsSidebar={setIsSidebar}
+                    setIsLoggedIn={setIsLoggedIn}
+                  />
+                )}
+                <Routes>
+                  <Route
+                    element={<PrivateRoutes setIsLoggedIn={setIsLoggedIn} />}
+                  >
+                    <Route path="/" element={<Dashboard />} />
+                    <Route path="/devices" element={<Devices />} />
+                    <Route path="/devices/:id" element={<Settings />} />
+                    <Route path="/faq" element={<FAQ />} />
+                    {/* <Route path="/test" element={<Test />} /> */}
+                  </Route>
+                  <Route
+                    path="/login"
+                    element={<Login setIsLoggedIn={setIsLoggedIn} />}
+                  />
+                  <Route path="/register" element={<Register />} />
+                </Routes>
+              </main>
+            </div>
+          </UserProvider>
+        </Provider>
       </ThemeProvider>
     </ColorModeContext.Provider>
   );
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -4,6 +4,40 @@ import "./index.css";
 import App from "./App";
 import { BrowserRouter } from "react-router-dom";
 import { QueryClient, QueryClientProvider } from "react-query";
+import axios from "axios";
+
+axios.interceptors.request.use(
+  (config) => {
+    const token = localStorage.getItem("token");
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`;
+    }
+    return config;
+  },
+  (error) => {
+    return Promise.reject(error);
+  }
+);
+
+axios.interceptors.response.use(
+  (response) => {
+    return response;
+  },
+  (error) => {
+    if (error.response.status === 401) {
+      if (window.location.pathname !== "/login") {
+        logoutUser();
+      }
+    }
+    return Promise.reject(error);
+  }
+);
+
+const logoutUser = () => {
+  localStorage.removeItem("token");
+  localStorage.setItem("isLoggedIn", false);
+  window.location.href = "/login";
+};
 
 const queryClient = new QueryClient();
 
