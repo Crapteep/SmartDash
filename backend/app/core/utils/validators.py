8c595b862c2c55b862c27f84b20ea52f8e411d89
Crapteep <crapteep@gmail.com>
2024-05-14 20:51:27 +0200

    new validator added

--- backend/app/core/utils/validators.py
+++ backend/app/core/utils/validators.py
@@ -1,14 +1,15 @@
 from bson import ObjectId
 import re
-from fastapi import HTTPException
+from fastapi import HTTPException, Path
 from .error_messages import ErrorMessages
+import datetime
 
 class Validator:
     @staticmethod
-    def is_valid_object_id(id: str):
-        if not ObjectId.is_valid(id):
+    def is_valid_object_id(id_: str =Path(...)):
+        if not ObjectId.is_valid(id_):
             raise HTTPException(400, detail=ErrorMessages.InvalidID)
-        return id
+        return id_
         
     @staticmethod
     def is_valid_pin(value: str):
@@ -17,3 +18,13 @@ class Validator:
         if re.match(regex_pattern, value):
             return value
         raise HTTPException(422, detail=ErrorMessages.InvalidPinFormat)
+    
+    @staticmethod
+    def is_valid_timestamp(time: float):
+        if not isinstance(time, float):
+            raise HTTPException(status_code=400, detail="Start time must be an integer representing a Unix timestamp in milliseconds")
+        try:
+            datetime.datetime.utcfromtimestamp(time / 1000.0)
+        except ValueError:
+            raise HTTPException(status_code=400, detail="Invalid timestamp. Must be a valid Unix timestamp in milliseconds")
+        return time
