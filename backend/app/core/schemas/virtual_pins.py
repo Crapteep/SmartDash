aa3615ff9a9830a95091d3aa714bd6660243229e
Crapteep <crapteep@gmail.com>
2024-05-14 20:45:23 +0200

    update fields for  virtual pin, add new validation and new model for available options

--- backend/app/core/schemas/virtual_pins.py
+++ backend/app/core/schemas/virtual_pins.py
@@ -1,27 +1,40 @@
-from pydantic import BaseModel, Field, constr
+from pydantic import BaseModel, Field, constr, validator
+from typing import Union, Optional, Literal
+from enum import Enum
 
 
 
-class VirtualPinInteger(BaseModel):
-    units: str | None = None
-    min_range: int = Field(default=0)
-    max_range: int = Field(default=1)
-    default: int | None = None
-
-
-class VirtualPinString(BaseModel):
-    default: str | None = None
-
-
-class VirtualPinBase(BaseModel):
+class VirtualPin(BaseModel):
     name: str
     pin: constr(min_length=2, max_length=4, regex=r'^V(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$')
-    data_type: str
-    value: int = 0
+    data_type: Literal["str", "int", "float"] = Field(alias="dataType")
+    color: constr(regex=r'^#([A-Fa-f0-9]{6})$')
+    min_range: Optional[Union[int, float]] = None
+    max_range: Optional[Union[int, float]] = None
+    value: Union[int, float, str]
+    legend_name: str | None = None
+    device_id: str
+
+    @validator("min_range", "max_range")
+    def validate_range(cls, v, values, **kwargs):
+        if v is not None and values.get("data_type") not in ["int", "float"]:
+            raise ValueError("min_range and max_range are only valid for data_type 'int' or 'float'")
+        return v
+    
+    @validator("value")
+    def validate_value(cls, v, values, **kwargs):
+        data_type = values.get("data_type")
+        if data_type == "int" and not isinstance(v, int):
+            raise ValueError("value must be an integer for data_type 'int'")
+        if data_type == "float" and not isinstance(v, float):
+            raise ValueError("value must be a float for data_type 'float'")
+        return v
 
 
-class VirtualPinIntegerCreate(VirtualPinBase):
-    option: VirtualPinInteger
-
-class VirtualPinStringCreate(VirtualPinBase):
-    option: VirtualPinString
+class QOptions(str, Enum):
+    chart = 'chart'
+    button = 'button'
+    slider = 'slider'
+    label = 'label'
+    diode = 'diode'
+    switch = 'switch'
