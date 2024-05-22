3310d181c8e1582fc3530a0e694a4a9922a14cb9
Crapteep <crapteep@gmail.com>
2024-05-22 19:25:35 +0200

    creation of a new element - slider and its settings

---
+++ frontend/src/components/elements/inputs/MySlider.jsx
@@ -0,0 +1,115 @@
+import React, { useState} from "react";
+import Slider from "@mui/material/Slider";
+import Typography from "@mui/material/Typography";
+import FormGroup from "@mui/material/FormGroup";
+import FormControlLabel from "@mui/material/FormControlLabel";
+import FormControl from "@mui/material/FormControl";
+import { useWebSocketContext } from "../../../providers/WebSocketProvider";
+
+const calculateWidth = (maxRange) => {
+  const digits = maxRange.toString().length;
+  return 50 * (digits + 1);
+};
+
+export default function SliderSwitch({
+  virtual_pins = [],
+  isEditMode,
+  step,
+  send_immediately,
+}) {
+  const { sendData } = useWebSocketContext();
+
+  const [sliderValue, setSliderValue] = useState(virtual_pins[0]?.value || 0);
+  const [pendingValue, setPendingValue] = useState();
+  const maxRange = virtual_pins[0]?.max_range || 0;
+  const labelWidth = calculateWidth(maxRange);
+
+  const containerStyle = {
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+    height: "100%",
+    width: "100%",
+    boxSizing: "border-box",
+  };
+
+  const sliderContainerStyle = {
+    display: "flex",
+    alignItems: "center",
+    width: "100%",
+    boxSizing: "border-box",
+  };
+  const labelStyle = {
+    fontSize: "32px",
+    width: `${labelWidth}px`,
+    textAlign: "right",
+  };
+
+  const handleSliderChange = (event, newValue) => {
+    setSliderValue(newValue);
+
+    if (send_immediately) {
+      sendValue(newValue);
+    } else {
+      setPendingValue(newValue);
+    }
+  };
+
+  const handleCommit = () => {
+    if (!send_immediately) {
+      sendValue(pendingValue);
+    }
+  };
+
+  const sendValue = (value) => {
+    if (virtual_pins && virtual_pins[0] && virtual_pins[0].pin) {
+      let valueToSend;
+      if (virtual_pins[0].data_type === "int") {
+        valueToSend = parseInt(value);
+      } else if (virtual_pins[0].data_type === "float") {
+        valueToSend = parseFloat(value);
+      }
+
+      const dataToSend = {
+        code: 1,
+        pin: virtual_pins[0].pin,
+        value: valueToSend,
+      };
+      console.log("wysylam");
+      sendData(dataToSend);
+    }
+  };
+
+  return (
+    <div style={containerStyle}>
+      <FormControl component="fieldset">
+        <FormGroup aria-label="position" row>
+          <FormControlLabel
+            style={sliderContainerStyle}
+            value="start"
+            control={
+              <Slider
+                value={sliderValue}
+                onChange={handleSliderChange}
+                onChangeCommitted={send_immediately ? null : handleCommit}
+                step={step}
+                min={virtual_pins[0]?.min_range}
+                max={maxRange}
+                aria-labelledby="continuous-slider"
+                style={{
+                  flexGrow: 1,
+                  width: "1000px",
+                  boxSizing: "border-box",
+                  marginLeft: "35px",
+                  marginRight: "15px",
+                }}
+                disabled={isEditMode}
+              />
+            }
+            label={<Typography style={labelStyle}>{sliderValue}</Typography>}
+          />
+        </FormGroup>
+      </FormControl>
+    </div>
+  );
+}
---
+++ frontend/src/components/elements/inputs/SliderSettings.jsx
@@ -0,0 +1,315 @@
+import React, { useState } from "react";
+import {
+  TextField,
+  Button,
+  Dialog,
+  DialogTitle,
+  DialogContent,
+  DialogActions,
+  FormControl,
+  InputLabel,
+  Select,
+  MenuItem,
+  ListItem,
+  ListItemText,
+  Box,
+  Grid,
+  FormControlLabel,
+  Checkbox,
+} from "@mui/material";
+import EditIcon from "@mui/icons-material/Edit";
+import DeleteIcon from "@mui/icons-material/Delete";
+import Tooltip from "@mui/material/Tooltip";
+import convertToNumber from "../../../functions/customConverter";
+
+const SliderSettings = ({ formData, handleChange, availablePins }) => {
+  const [pinDialogOpen, setPinDialogOpen] = useState(false);
+  const [selectedPin, setSelectedPin] = useState(null);
+  const [pinNumber, setPinNumber] = useState("");
+  const [hoveredPin, setHoveredPin] = useState(null);
+  const [variant, setVariant] = useState(formData.variant);
+  const dataType =
+    formData.virtual_pins &&
+    formData.virtual_pins[0] &&
+    formData.virtual_pins[0].data_type;
+  const minRange =
+    formData.virtual_pins &&
+    formData.virtual_pins[0] &&
+    formData.virtual_pins[0].min_range;
+  const maxRange =
+    formData.virtual_pins &&
+    formData.virtual_pins[0] &&
+    formData.virtual_pins[0].max_range;
+
+  const handlePinDialogOpen = (pin) => {
+    setSelectedPin(pin);
+    setPinNumber(pin);
+    setPinDialogOpen(true);
+  };
+  const handlePinDialogClose = () => {
+    setSelectedPin(null);
+    setPinDialogOpen(false);
+    setPinNumber("");
+  };
+
+  const handlePinAction = () => {
+    if (selectedPin) {
+      handleEditPin(selectedPin, pinNumber);
+    } else {
+      handleAddPin(pinNumber);
+    }
+    handleChange("step", 1)
+  };
+
+  const handleAddPin = () => {
+    console.log("dodawania");
+    const pinCount = formData.virtual_pins.length;
+    if (pinCount < 1) {
+      const selectedPin = availablePins.find((pin) => pin.pin === pinNumber);
+
+      if (selectedPin) {
+        handleChange("virtual_pins", [...formData.virtual_pins, selectedPin]);
+      } else {
+        console.error(
+          `Pin ${pinNumber} nie został znaleziony w dostępnych pinach.`
+        );
+      }
+    } else {
+      console.warn("Osiągnięto maksymalną liczbę pinów (1).");
+    }
+
+    handlePinDialogClose();
+  };
+
+  const handleEditPin = (oldPin, newPin) => {
+    console.log("edycja");
+    const editedPinIndex = formData.virtual_pins.findIndex(
+      (element) => element.pin === oldPin
+    );
+    if (editedPinIndex !== -1) {
+      const selectedPin = availablePins.find(
+        (element) => element.pin === newPin
+      );
+
+      if (selectedPin) {
+        formData.virtual_pins[editedPinIndex] = {
+          ...selectedPin,
+        };
+
+        handleChange("virtual_pins", [...formData.virtual_pins]);
+        console.log("moze to??", formData.virtual_pins);
+      } else {
+        console.error(
+          `Pin ${pinName} nie został znaleziony w dostępnych pinach.`
+        );
+      }
+    } else {
+      console.error(`Nie można znaleźć pinu`);
+    }
+
+    handlePinDialogClose();
+  };
+
+  const handleInputChange = (event) => {
+    const { name, value } = event.target;
+    console.log(name, value)
+    handleChange(name, value)
+  };
+
+  const handleDeletePin = (pinName) => {
+    const updatedPins = formData.virtual_pins.filter(
+      (pin) => pin.pin !== pinName
+    );
+    handleChange("virtual_pins", updatedPins);
+  };
+
+  const isInteger = (value) => {
+    return parseFloat(value) === parseInt(value, 10) && !isNaN(value);
+  };
+
+  const roundToTwoDecimalPlaces = (value) => {
+    return Math.round((value + Number.EPSILON) * 100) / 100;
+  };
+
+  const getStep = (dataType, minRange, maxRange) => {
+    if (dataType === "float") {
+      return "any";
+    } else {
+      return 1;
+    }
+  };
+
+  const handleStepChange = (event) => {
+    let value = event.target.value;
+    console.log('data type', dataType)
+    if (dataType === "int") {
+      value = Math.round(parseFloat(value));
+    } else {
+      value = roundToTwoDecimalPlaces(parseFloat(value));
+    }
+    if (value < minRange) {
+      value = minRange;
+    } else if (value > maxRange) {
+      value = maxRange;
+    }
+    console.log(event)
+    handleChange("step", value)
+    formData.step = value;
+
+  };
+
+  return (
+    <>
+      <div>
+        {!formData.virtual_pins || formData.virtual_pins.length === 0 ? (
+          <Tooltip title="Add a new pin" enterDelay={500}>
+            <Button onClick={() => handlePinDialogOpen()} color="primary">
+              Add pin
+            </Button>
+          </Tooltip>
+        ) : null}
+      </div>
+
+      {formData.virtual_pins.map((element, index) => (
+        <ListItem
+          key={index}
+          onMouseEnter={() => setHoveredPin(element)}
+          onMouseLeave={() => setHoveredPin(null)}
+          style={{
+            display: "flex",
+            alignItems: "center",
+            border: "1px solid #ccc",
+            borderRadius: "10px",
+            marginBottom: "5px",
+          }}
+        >
+          <ListItemText primary={`${element.pin}`} />
+          {hoveredPin === element && (
+            <div
+              style={{
+                display: "flex",
+                alignItems: "center",
+                justifyContent: "space-between",
+              }}
+            >
+              <Tooltip title="Edit pin" enterDelay={500}>
+                <EditIcon onClick={() => handlePinDialogOpen(element.pin)} />
+              </Tooltip>
+              <Tooltip title="Delete pin" enterDelay={500}>
+                <DeleteIcon onClick={() => handleDeletePin(element.pin)} />
+              </Tooltip>
+            </div>
+          )}
+        </ListItem>
+      ))}
+
+      <Dialog open={pinDialogOpen} onClose={handlePinDialogClose}>
+        <DialogTitle>{selectedPin ? "Edit PIN" : "Add PIN"}</DialogTitle>
+        <DialogContent>
+          <FormControl fullWidth>
+            <InputLabel id="pinNumber-label">
+              {pinNumber ? "" : "Select PIN"}
+            </InputLabel>
+            <Select
+              labelId="pinNumber-label"
+              value={pinNumber || ""}
+              onChange={(e) => setPinNumber(e.target.value)}
+              style={{ maxHeight: 200 }}
+              MenuProps={{ PaperProps: { style: { maxHeight: 300 } } }}
+            >
+              {availablePins &&
+                availablePins.map((pin) => (
+                  <MenuItem key={pin._id} value={pin.pin}>
+                    {pin.pin}
+                  </MenuItem>
+                ))}
+            </Select>
+          </FormControl>
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={handlePinDialogClose} color="primary">
+            Cancel
+          </Button>
+          <Button onClick={handlePinAction} color="primary">
+            {selectedPin ? "Edit" : "Add"}
+          </Button>
+        </DialogActions>
+      </Dialog>
+
+      <Box my={4} />
+
+      <Grid container alignItems="center" spacing={2}>
+        <Grid item xs={6}>
+          <TextField
+            name="widget_title"
+            label="Widget Title"
+            value={formData.widget_title}
+            onChange={handleInputChange}
+            fullWidth
+          />
+        </Grid>
+
+        <Grid item xs={6}>
+          <TextField
+            name="alias"
+            label="Alias"
+            value={formData.alias}
+            onChange={handleInputChange}
+            fullWidth
+          />
+        </Grid>
+      </Grid>
+
+      {formData.virtual_pins.length > 0 && (
+        <>
+          <Box my={2} />
+          <Grid container alignItems="center" spacing={2}>
+            <Grid item xs={6}>
+              <TextField
+                name="step"
+                label="Handle step"
+                value={formData.step}
+                onChange={handleStepChange}
+                type="number"
+                fullWidth
+                InputProps={{
+                  inputProps: {
+                    min: minRange,
+                    step: getStep(dataType, minRange, maxRange),
+                    max: maxRange,
+                  },
+                  ...(dataType === "int" && { inputMode: "numeric" }),
+                }}
+              />
+            </Grid>
+
+            <Grid item xs={6}>
+              <FormControlLabel
+                control={
+                  <Checkbox
+                    checked={formData.send_immediately}
+                    onChange={(event) => {
+                      const customEvent = {
+                        ...event,
+                        target: {
+                          ...event.target,
+                          name: "send_immediately",
+                          value: event.target.checked,
+                        },
+                      };
+                      handleInputChange(customEvent);
+                    }}
+                    name="send_immediately"
+                  />
+                }
+                label="Send immediately"
+              />
+            </Grid>
+          </Grid>
+        </>
+      )}
+    </>
+  );
+};
+
+export default SliderSettings;
