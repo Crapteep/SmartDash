b36d87bae84c8cacff295c8e9d8cc6cb31550089
Crapteep <crapteep@gmail.com>
2024-07-13 11:45:10 +0200

    Redesigned login and registration page (available on the homepage from now on)

--- frontend/src/pages/login/index.jsx
+++ frontend/src/pages/login/index.jsx
@@ -2,36 +2,30 @@ import { useState } from "react";
 import React from "react";
 import Avatar from "@mui/material/Avatar";
 import Button from "@mui/material/Button";
-import CssBaseline from "@mui/material/CssBaseline";
 import TextField from "@mui/material/TextField";
 import FormControlLabel from "@mui/material/FormControlLabel";
 import Checkbox from "@mui/material/Checkbox";
-import { Link } from "react-router-dom";
 import Grid from "@mui/material/Grid";
 import Box from "@mui/material/Box";
 import LockOutlinedIcon from "@mui/icons-material/LockOutlined";
 import Typography from "@mui/material/Typography";
-import Container from "@mui/material/Container";
-import { createTheme, ThemeProvider } from "@mui/material/styles";
 import axios from "axios";
-import { Navigate } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 
-const defaultTheme = createTheme();
-
-export default function Login({ setIsLoggedIn }) {
+export default function Login({ setIsLoggedIn, onClose, switchToRegister }) {
   const URL = import.meta.env.VITE_APP_API_URL;
-  const [redirectToDashboard, setRedirectToDashboard] = useState(false);
   const [loginDisplayError, setLoginDisplayError] = useState("");
   const [formData, setFormData] = useState({
     username: "",
     password: "",
   });
+  const navigate = useNavigate();
 
   const handleSubmit = (e) => {
     e.preventDefault();
 
     axios
-      .post(`${URL}/api/v1/token/`, formData, {
+      .post(`${URL}/token/`, formData, {
         headers: {
           Accept: "application/json",
           "Content-Type": "application/x-www-form-urlencoded",
@@ -39,22 +33,15 @@ export default function Login({ setIsLoggedIn }) {
       })
       .then((response) => {
         localStorage.setItem("token", response.data.access_token);
-        setRedirectToDashboard(true);
         setIsLoggedIn(true);
-        resetForm();
+        onClose();
+        navigate("/dashboard");
       })
       .catch((error) => {
         if (error.response && error.response.status === 401) {
           setLoginDisplayError(error.response.data.detail);
         }
       });
-  };
-
-  const resetForm = () => {
-    setFormData({
-      username: "",
-      password: "",
-    });
   };
 
   const handleChange = (e) => {
@@ -71,92 +58,84 @@ export default function Login({ setIsLoggedIn }) {
     }
   };
 
-  if (redirectToDashboard) {
-    return <Navigate to="/" />;
-  } else
-    return (
-      <ThemeProvider theme={defaultTheme}>
-        <Container component="main" maxWidth="xs" onClick={handleMouseClick}>
-          <CssBaseline />
-          <Box
-            sx={{
-              marginTop: 8,
-              display: "flex",
-              flexDirection: "column",
-              alignItems: "center",
-            }}
-          >
-            <Avatar sx={{ m: 1, bgcolor: "secondary.main" }}>
-              <LockOutlinedIcon />
-            </Avatar>
-            <Typography component="h1" variant="h5">
-              Sign in
+  return (
+    <Box
+      sx={{
+        marginTop: 3,
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center",
+      }}
+      onClick={handleMouseClick}
+    >
+      <Avatar sx={{ m: 1, bgcolor: "secondary.main" }}>
+        <LockOutlinedIcon />
+      </Avatar>
+      <Typography component="h1" variant="h5">
+        Sign in
+      </Typography>
+      <Box component="form" onSubmit={handleSubmit} noValidate sx={{ mt: 1 }}>
+        <Box>
+          {loginDisplayError ? (
+            <Typography variant="body1" color="error" gutterBottom>
+              {loginDisplayError}
             </Typography>
-            <Box
-              component="form"
-              onSubmit={handleSubmit}
-              noValidate
-              sx={{ mt: 1 }}
+          ) : (
+            <div style={{ height: 25 }} />
+          )}
+        </Box>
+        <TextField
+          margin="normal"
+          required
+          fullWidth
+          id="username"
+          label="Username"
+          name="username"
+          autoComplete="username"
+          onChange={handleChange}
+          autoFocus
+        />
+        <TextField
+          margin="normal"
+          required
+          fullWidth
+          name="password"
+          label="Password"
+          type="password"
+          id="password"
+          onChange={handleChange}
+          autoComplete="current-password"
+        />
+        <FormControlLabel
+          control={<Checkbox value="remember" color="primary" />}
+          label="Remember me"
+        />
+        <Button
+          type="submit"
+          fullWidth
+          variant="contained"
+          sx={{ mt: 3, mb: 2 }}
+        >
+          Sign In
+        </Button>
+        <Grid container>
+          <Grid item xs>
+            <Typography variant="body2" component="span">
+              Forgot password?
+            </Typography>
+          </Grid>
+          <Grid item>
+            <Typography
+              variant="body2"
+              component="span"
+              onClick={switchToRegister}
+              sx={{ cursor: "pointer", color: "primary.main" }}
             >
-              <Box>
-                {/* Display information */}
-                {loginDisplayError ? (
-                  <Typography variant="body1" color="error" gutterBottom>
-                    {loginDisplayError}
-                  </Typography>
-                ) : (
-                  <div style={{ height: 30 }} />
-                )}
-              </Box>
-              <TextField
-                margin="normal"
-                required
-                fullWidth
-                id="username"
-                label="Username"
-                name="username"
-                autoComplete="username"
-                onChange={handleChange}
-                autoFocus
-              />
-              <TextField
-                margin="normal"
-                required
-                fullWidth
-                name="password"
-                label="Password"
-                type="password"
-                id="password"
-                onChange={handleChange}
-                autoComplete="current-password"
-              />
-              <FormControlLabel
-                control={<Checkbox value="remember" color="primary" />}
-                label="Remember me"
-              />
-              <Button
-                type="submit"
-                fullWidth
-                variant="contained"
-                sx={{ mt: 3, mb: 2 }}
-              >
-                Sign In
-              </Button>
-              <Grid container>
-                <Grid item xs>
-                  <Link href="#" variant="body2">
-                    Forgot password?
-                  </Link>
-                </Grid>
-                <Grid item>
-                  <Link to="/register" variant="body2">
-                    {"Don't have an account? Sign Up"}
-                  </Link>
-                </Grid>
-              </Grid>
-            </Box>
-          </Box>
-        </Container>
-      </ThemeProvider>
-    );
+              {"Don't have an account? Sign Up"}
+            </Typography>
+          </Grid>
+        </Grid>
+      </Box>
+    </Box>
+  );
 }
--- frontend/src/pages/register/index.jsx
+++ frontend/src/pages/register/index.jsx
@@ -1,34 +1,19 @@
-import React from "react";
+import React, { useState } from "react";
 import Avatar from "@mui/material/Avatar";
 import Button from "@mui/material/Button";
-import CssBaseline from "@mui/material/CssBaseline";
 import TextField from "@mui/material/TextField";
 import FormControlLabel from "@mui/material/FormControlLabel";
 import Checkbox from "@mui/material/Checkbox";
-import { Link } from "react-router-dom";
 import Grid from "@mui/material/Grid";
 import Box from "@mui/material/Box";
 import LockOutlinedIcon from "@mui/icons-material/LockOutlined";
 import Typography from "@mui/material/Typography";
-import Container from "@mui/material/Container";
-import { createTheme, ThemeProvider } from "@mui/material/styles";
-import { useState } from "react";
 import axios from "axios";
-import { Navigate } from "react-router-dom";
+import { Dialog, DialogTitle, DialogContent, DialogActions } from "@mui/material";
 import { BsFillPersonCheckFill } from "react-icons/bs";
 
-import {
-  Dialog,
-  DialogTitle,
-  DialogContent,
-  DialogActions,
-} from "@mui/material";
-
-const defaultTheme = createTheme();
-
-export default function Register() {
+export default function Register({ onClose, switchToLogin }) {
   const URL = import.meta.env.VITE_APP_API_URL;
-  const [redirectToLogin, setRedirectToLogin] = useState(false);
   const [emailDisplayError, setEmailDisplayError] = useState("");
   const [openDialog, setOpenDialog] = useState(false);
   const [formData, setFormData] = useState({
@@ -44,9 +29,7 @@ export default function Register() {
         ...formData,
       })
       .then((response) => {
-        setRedirectToLogin(true);
         setOpenDialog(true);
-        resetForm();
       })
       .catch((error) => {
         if (error.response && error.response.status === 409) {
@@ -60,14 +43,7 @@ export default function Register() {
 
   const handleDialogClose = () => {
     setOpenDialog(false);
-  };
-
-  const resetForm = () => {
-    setFormData({
-      email: "",
-      username: "",
-      password: "",
-    });
+    onClose();
   };
 
   const handleChange = (e) => {
@@ -84,124 +60,113 @@ export default function Register() {
     }
   };
 
-  if (redirectToLogin) {
-    return <Navigate to="/login" />;
-  } else
-    return (
-      <ThemeProvider theme={defaultTheme}>
-        <Container component="main" maxWidth="xs" onClick={handleMouseClick}>
-          <CssBaseline />
-          <Box
-            sx={{
-              marginTop: 8,
-              display: "flex",
-              flexDirection: "column",
-              alignItems: "center",
-            }}
-          >
-            <Avatar sx={{ m: 1, bgcolor: "secondary.main" }}>
-              <LockOutlinedIcon />
-            </Avatar>
-            <Typography component="h1" variant="h5">
-              Sign up
-            </Typography>
-            <Box
-              component="form"
-              noValidate
-              onSubmit={handleSubmit}
-              sx={{ mt: 3 }}
-            >
-              <Grid container spacing={2}>
-                <Grid item xs={12}>
-                  <Box>
-                    {/* Display information */}
-                    {emailDisplayError && (
-                      <Typography variant="body1" color="error" gutterBottom>
-                        {emailDisplayError}
-                      </Typography>
-                    )}
-                  </Box>
-                  <TextField
-                    autoComplete="username"
-                    name="username"
-                    required
-                    fullWidth
-                    id="username"
-                    label="Username"
-                    onChange={handleChange}
-                    autoFocus
-                  />
-                </Grid>
-                <Grid item xs={12}>
-                  <TextField
-                    required
-                    fullWidth
-                    id="email"
-                    label="Email Address"
-                    name="email"
-                    onChange={handleChange}
-                    autoComplete="email"
-                  />
-                </Grid>
-                <Grid item xs={12}>
-                  <TextField
-                    required
-                    fullWidth
-                    name="password"
-                    label="Password"
-                    type="password"
-                    id="password"
-                    onChange={handleChange}
-                    autoComplete="new-password"
-                  />
-                </Grid>
-                <Grid item xs={12}>
-                  <FormControlLabel
-                    control={
-                      <Checkbox value="allowExtraEmails" color="primary" />
-                    }
-                    label="I want to receive inspiration, marketing promotions and updates via email."
-                  />
-                </Grid>
-              </Grid>
-              <Button
-                type="submit"
-                fullWidth
-                variant="contained"
-                sx={{ mt: 3, mb: 2 }}
-              >
-                Sign Up
-              </Button>
-              <Grid container justifyContent="flex-end">
-                <Grid item>
-                  <Link to="/login" variant="body2">
-                    {"Don't have an account? Sign Up"}
-                  </Link>
-                </Grid>
-              </Grid>
+  return (
+    <Box
+      sx={{
+        marginTop: 3,
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center",
+      }}
+      onClick={handleMouseClick}
+    >
+      <Avatar sx={{ m: 1, bgcolor: "secondary.main" }}>
+        <LockOutlinedIcon />
+      </Avatar>
+      <Typography component="h1" variant="h5">
+        Sign up
+      </Typography>
+      <Box component="form" noValidate onSubmit={handleSubmit} sx={{ mt: 3 }}>
+        <Grid container spacing={2}>
+          <Grid item xs={12}>
+            <Box>
+              {emailDisplayError ? (
+                <Typography variant="body1" color="error" gutterBottom>
+                  {emailDisplayError}
+                </Typography>
+              ): (
+                <div style={{ height: 25 }} />
+              )}
             </Box>
-          </Box>
-          {/* Success Dialog */}
-          <Dialog open={openDialog} onClose={handleDialogClose}>
-            <DialogTitle className>
-              Success
-              <BsFillPersonCheckFill
-                icon="fa-solid fa-check"
-                className="ms-2"
-              />
-            </DialogTitle>
-            <DialogContent>
-              <Typography variant="body1">
-                The account has been successfully added!
-              </Typography>
-            </DialogContent>
-            <DialogActions>
-              <Button onClick={handleDialogClose} color="primary">
-                OK
-              </Button>
-            </DialogActions>
-          </Dialog>
-        </Container>
-      </ThemeProvider>
-    );
+            <TextField
+              autoComplete="username"
+              name="username"
+              required
+              fullWidth
+              id="username"
+              label="Username"
+              onChange={handleChange}
+              autoFocus
+            />
+          </Grid>
+          <Grid item xs={12}>
+            <TextField
+              required
+              fullWidth
+              id="email"
+              label="Email Address"
+              name="email"
+              onChange={handleChange}
+              autoComplete="email"
+            />
+          </Grid>
+          <Grid item xs={12}>
+            <TextField
+              required
+              fullWidth
+              name="password"
+              label="Password"
+              type="password"
+              id="password"
+              onChange={handleChange}
+              autoComplete="new-password"
+            />
+          </Grid>
+          <Grid item xs={12}>
+            <FormControlLabel
+              control={<Checkbox value="allowExtraEmails" color="primary" />}
+              label="I want to receive inspiration, marketing promotions and updates via email."
+            />
+          </Grid>
+        </Grid>
+        <Button
+          type="submit"
+          fullWidth
+          variant="contained"
+          sx={{ mt: 3, mb: 2 }}
+        >
+          Sign Up
+        </Button>
+        <Grid container justifyContent="flex-end">
+          <Grid item>
+            <Typography
+              variant="body2"
+              component="span"
+              onClick={switchToLogin}
+              sx={{ cursor: 'pointer', color: 'primary.main' }}
+            >
+              {"Already have an account? Sign in"}
+            </Typography>
+          </Grid>
+        </Grid>
+      </Box>
+      {/* Success Dialog */}
+      <Dialog open={openDialog} onClose={handleDialogClose}>
+        <DialogTitle>
+          Success
+          <BsFillPersonCheckFill className="ms-2" />
+        </DialogTitle>
+        <DialogContent>
+          <Typography variant="body1">
+            The account has been successfully added!
+          </Typography>
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={handleDialogClose} color="primary">
+            OK
+          </Button>
+        </DialogActions>
+      </Dialog>
+    </Box>
+  );
 }
